9aebe94120b72ccad8353c512fa844855031932d
unknown <amirahmed7208@gmail.com>
2026-03-06 03:59:37 +0600

    fifth commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDssk-m8ePMHvjuQ3bPSLHecPsQfw0-yZY",
+  authDomain: "pet-care-3757f.firebaseapp.com",
+  projectId: "pet-care-3757f",
+  storageBucket: "pet-care-3757f.firebasestorage.app",
+  messagingSenderId: "758814123314",
+  appId: "1:758814123314:web:4883e1e359ff6301b8632b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
